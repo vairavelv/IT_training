26181f14ef23fd8d3cc4c748275959944497d335
vairavelv <vairavelv00@gmail.com>
2026-07-03 09:38:48 +0530

    first changes

---
+++ IT_training/src/Day_7/home_task/Rotate_array.java
@@ -0,0 +1,23 @@
+package Day_7.home_task;
+
+public class Rotate_array{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        int d = 1; 
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + d) % n] = arr[i];
+        }
+
+        
+        System.arraycopy(temp, 0, arr, 0, n);
+
+        System.out.println("Rotated array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
